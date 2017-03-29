5d831114239a8466b6f5fdaa12feb15e97be9a55
Diamant <diamantkolshi@gmail.com>
2017-03-29 14:00:00 +0100

    modify stylesheet, when input has code

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -2,7 +2,7 @@ class RoomChannel < ApplicationCable::Channel
   def subscribed
     stream_from "room_channel"
   end
-
+  
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
